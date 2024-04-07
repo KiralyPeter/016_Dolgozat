a22e33798a55b15c08ad3f4f7e784a95c37d6f1a
KiralyPeter <peter.kiral@gmail.com>
2024-04-07 10:48:00 +0200

    apróbb módosítás

---
+++ mobil/App.js
@@ -0,0 +1,60 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { FlatList, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { getStudents } from './services/tanulokService';
+
+
+
+export default function App() {
+
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents().then((data) => {
+      console.log(data)
+      setStudents(data)
+    })
+  }, []);
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Tanulók listája</Text>
+
+      <FlatList
+        data={students}
+        renderItem={({ item }) => (
+          <View style={styles.items}>            
+            <Text>Tanuló neve: {item.name}</Text>
+            <Text>Város: {item.city}</Text>
+            <Text>Születési dátum: {item.birth}</Text>            
+          </View>
+        )}
+
+      />
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  items: {
+    border: 'solid 1px black',
+    marginTop: 10,
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: '#6ee263',
+    boxShadow: '5px 5px 5px gray',
+  }, 
+  header: {
+    fontSize: '22px',
+  },
+});
